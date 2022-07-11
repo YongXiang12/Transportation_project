fa51d47f3f540b2d10f7810542c163cc92de5b71
xyu1015 <song0986145115@gmail.com>
2022-07-11 17:25:46 +0800

    可以用的

--- lib/DB_Sqlite/UserSelectEntity.dart
+++ lib/DB_Sqlite/UserSelectEntity.dart
@@ -1,4 +1,4 @@
-class UserSelectEntity{
+/*class UserSelectEntity{
 
   final String t1 ;
 
@@ -10,4 +10,4 @@ class UserSelectEntity{
   });
 
 
-}
+}*/
--- lib/setting/record.dart
+++ lib/setting/record.dart
@@ -5,7 +5,8 @@ import 'package:flutter/services.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:audioplayers/audioplayers.dart';
-import 'package:transport/DB_Sqlite/UserEntity.dart';
+
+import '../DB_Sqlite/UserEntity.dart';
 
 class record_page extends StatefulWidget {
   record_page({Key? key, required this.data}) : super(key: key);
